338736772ff2a6301c8f09eb2ca475a9cdaff85b
Munshi91 <54263790+Munshi91@users.noreply.github.com>
2023-06-21 08:10:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var settingsmenu = document.querySelector(".settings__menu");
 
+var darkBtn = document.getElementById("dark__btn");
 function settingsMenuToggle() {
   settingsmenu.classList.toggle("settings__menu-height");
 }
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark__btn-on");
+};
