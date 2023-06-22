9d2d43f48ee722157a1ba1ddf15448929307c4b3
Munshi91 <54263790+Munshi91@users.noreply.github.com>
2023-06-22 06:37:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ function settingsMenuToggle() {
 }
 darkBtn.onclick = function () {
   darkBtn.classList.toggle("dark__btn-on");
+  document.body.classList.toggle("dark__theme");
+
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 };
+
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark__btn-on");
+  document.body.classList.remove("dark__theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark__btn-on");
+  document.body.classList.add("dark__theme");
+} else {
+  localStorage.setItem("theme", "light");
+}
+
+// localStorage.setItem("theme", "dark");
+// localStorage.getItem("theme");
